f970897e3a9ad76a6ae4fb7aaf48ec90811fe26c
Andrew Henry <andrewmhenry@me.com>
2021-07-10 13:39:30 +0800

    :rocket: config and creation of the subgraph that indexes the Zora API

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,66 +1,45 @@
 import {
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
-  TokenMetadataURIUpdated as TokenMetadataURIUpdatedEvent,
   TokenURIUpdated as TokenURIUpdatedEvent,
-  Transfer as TransferEvent
-} from "../generated/Token/Token"
-import {
-  Approval,
-  ApprovalForAll,
-  TokenMetadataURIUpdated,
-  TokenURIUpdated,
-  Transfer
-} from "../generated/schema"
+  Transfer as TransferEvent,
+  Token as TokenContract,
+} from '../generated/Token/Token'
 
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
-  entity.save()
-}
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-  entity.save()
-}
-
-export function handleTokenMetadataURIUpdated(
-  event: TokenMetadataURIUpdatedEvent
-): void {
-  let entity = new TokenMetadataURIUpdated(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._tokenId = event.params._tokenId
-  entity.owner = event.params.owner
-  entity._uri = event.params._uri
-  entity.save()
-}
-
-export function handleTokenURIUpdated(event: TokenURIUpdatedEvent): void {
-  let entity = new TokenURIUpdated(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._tokenId = event.params._tokenId
-  entity.owner = event.params.owner
-  entity._uri = event.params._uri
-  entity.save()
-}
+import { Token, User } from '../generated/schema'
 
 export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
-  entity.save()
+  // load the token from the graph node
+  let token = Token.load(event.params.tokenId.toString())
+
+  // create a new token if it doesn't exist
+  if (!token) {
+    token = new Token(event.params.tokenId.toString())
+    token.creator = event.params.to.toHexString()
+    token.tokenID = event.params.tokenId
+    token.createdAtTimestamp = event.block.timestamp
+
+    // call out to the token contract to get and set the content URI and metadata URI
+    let tokenContract = TokenContract.bind(event.address)
+    token.contentURI = tokenContract.tokenURI(event.params.tokenId)
+    token.metadataURI = tokenContract.tokenMetadataURI(event.params.tokenId)
+  }
+
+  // set the token owner and save it to the store
+  token.owner = event.params.to.toHexString()
+  token.save()
+
+  // check to see if the user exists
+  let user = User.load(event.params.to.toHexString())
+  // if not, create a new user
+  if (!user) {
+    user = new User(event.params.to.toHexString())
+    user.save()
+  }
+}
+
+// update the content URI of the token
+// then save it back to the store
+export function handleTokenURIUpdated(event: TokenURIUpdatedEvent): void {
+  let token = Token.load(event.params._tokenId.toString())
+  token.contentURI = event.params._uri
+  token.save()
 }
